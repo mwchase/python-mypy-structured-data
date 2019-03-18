13769988241dd45383902365122a171996da77cf
Max Chase <max.chase@gmail.com>
2019-03-17 22:05:18 -0400

    Add do-nothing plugin class

--- src/mypy_structured_data/__init__.py
+++ src/mypy_structured_data/__init__.py
@@ -1,1 +1,12 @@
+from mypy.plugin import Plugin
+
 __version__ = "0.1.1"
+
+
+class StructuredDataPlugin(Plugin):
+    pass
+
+
+def plugin(version: str):
+    # ignore version argument if the plugin works with all mypy versions.
+    return StructuredDataPlugin
--- tests/test_mypy_structured_data.py
+++ tests/test_mypy_structured_data.py
@@ -1,2 +1,10 @@
+from mypy.plugin import Plugin
+from mypy.version import __version__
+
+
 def test_main(mypy_structured_data):
     assert mypy_structured_data
+
+
+def test_plugin(mypy_structured_data):
+    assert issubclass(mypy_structured_data.plugin(__version__), Plugin)
